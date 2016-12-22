b1d78dad1eb0b0e5ea98a413b690997d3d2fcd69
USER <sanya.groza@yandex.ua>
2016-12-22 19:31:50 +0200

    Homework 7-8

--- script.js
+++ script.js
@@ -1,129 +1,137 @@
-var msek = 0;
-var sek = 0;
-var min = 0;
-var hr = 0;
-var msI;
-var sI;
-var mI;
-var hI;
+$(function() {
 
-// Считает милисекунды:
-function ms() {
-  msek++;
-  if(msek > 999) {
-    msek = 0;
-  }
-  if(msek < 10) {
-    msek = '00' + msek;
-  } else if(msek < 100) {
-    msek = '0' + msek;
-  } else {
-    msek = msek;
-  };
-  var millisecond = document.getElementById('millisecond');
-    millisecond.innerHTML = msek;
-  };
+///////// TABS /////////////
 
-//  Считает секунды:
-  function s() {
-    sek++;
-    if(sek > 59) {
-      sek = 0;
-    }
-    if(sek < 10) {
-      sek = '0' + sek;
-    };
-    var second = document.getElementById('second');
-    second.innerHTML = sek;
-  };
+  var $text = $('#text');
+  var $style = $('#style');
+  var $script = $('#script');
 
-// Считает минуты:
-  function m() {
-    min++;
-    if(min > 59) {
-      min = 0;
-    }
-    if(min < 10) {
-      min = '0' + min;
-    };
-    var minutes = document.getElementById('minutes');
-    minutes.innerHTML = min;
-  };
+  var $one = $('.one');
+  var $two = $('.two');
+  var $three = $('.three');
 
-// Считает часы:
-  function h() {
-    hr++;
-    var hour = document.getElementById('hour');
-    hour.innerHTML = hr;
-  };
+  $text.on('click', function() {
+    $one.css({
+      display: 'block'
+    });
+    $two.css({
+      display: 'none'
+    });
+    $three.css({
+      display: 'none'
+    });
+    $text.css({
+      borderBottom: '5px solid #e6e6e6'
+    });
+    $style.css({
+      borderBottom: 'none'
+    });
+    $script.css({
+      borderBottom: 'none'
+    })
+  });
 
-// Запускает таймер:
-function timer() {
-   msI = setInterval(ms, 1);
-   sI = setInterval(s, 1000);
-   mI = setInterval(m, 60000);
-   hI = setInterval(h, 3600000);
-   start.innerHTML = 'pause';
-   k++;
-}
+  $style.on('click', function() {
+    $one.css({
+      display: 'none'
+    });
+    $two.css({
+      display: 'block'
+    });
+    $three.css({
+      display: 'none'
+    });
+    $text.css({
+      borderBottom: 'none'
+    });
+    $style.css({
+      borderBottom: '5px solid #e6e6e6'
+    });
+    $script.css({
+      borderBottom: 'none'
+    })
+  });
 
-// Делает паузу:
-function timeout() {
-    clearInterval(msI);
-    clearInterval(sI);
-    clearInterval(mI);
-    clearInterval(hI);
-    start.innerHTML = 'start';
-    k++;
-  }
+  $script.on('click', function() {
+    $one.css({
+      display: 'none'
+    });
+    $two.css({
+      display: 'none'
+    });
+    $three.css({
+      display: 'block'
+    });
+    $text.css({
+      borderBottom: 'none'
+    });
+    $style.css({
+      borderBottom: 'none'
+    });
+    $script.css({
+      borderBottom: '5px solid #e6e6e6'
+    })
+  });
 
-// Кнопка START/PAUSE:
-  var k = 0;
-  var start = document.getElementById('start');
-  start.onclick = function() {
-    if(k % 2 == 0) {
-      timer();
-    } else {
-      timeout();
-      scrin();
-    }
-}
+///////// FORM /////////////
 
-// Обнуляет время:
-var container = document.getElementById('container');
-  function res() {
-    msek = 0;
-    sek = 0;
-    min = 0;
-    hr = 0;
-    millisecond.innerHTML = '000';
-    second.innerHTML = '00';
-    minutes.innerHTML = '00';
-    hour.innerHTML = '00';
-    k = 1;
-    j = 0;
-    container.remove();
-    container = document.createElement('div');
-    var body = document.body;
-    body.appendChild(container);
-    timeout();
-  }
+var $firstname = $('.firstname');
+var $lastname = $('.lastname');
+var $address = $('.address');
 
-//  Кнопка RESET:
-  var reset = document.getElementById('reset');
-  reset.onclick = res;
+var $firstnameHelp = $('.firstnameHelp');
+var $lastnameHelp = $('.lastnameHelp');
+var $addressHelp = $('.addressHelp');
 
-// Делает скрин:
-  var text;
-  var j=0;
-  function scrin() {
-    j++;
-    text = document.createElement('p');
-    text.className = ('scrin');
-    text.innerHTML = j + ' Split: ' + hour.innerHTML + ':' + minutes.innerHTML + ':' + second.innerHTML + '.' + millisecond.innerHTML;
-    container.appendChild(text);
-  }
+var $button = $('button');
 
-  // Кнопка SPLIT:
-  var split = document.getElementById('split');
-  split.onclick = scrin;
+function over1() {
+  $firstnameHelp.animate({
+    opacity: 0.7
+  }, 100)};
+
+function out1() {
+  $firstnameHelp.animate({
+    opacity: 0
+  }, 100)};
+
+
+function over2() {
+  $lastnameHelp.animate({
+    opacity: 0.7
+  }, 100)};
+
+function out2() {
+  $lastnameHelp.animate({
+    opacity: 0
+  }, 100)};
+
+
+function over3() {
+  $addressHelp.animate({
+    opacity: 0.7
+  }, 100)};
+
+function out3() {
+  $addressHelp.animate({
+    opacity: 0
+  }, 100)};
+
+$firstname.hover(over1, out1);
+$lastname.hover(over2, out2);
+$address.hover(over3, out3);
+
+$firstname.focus(over1);
+$firstname.focusout(out1);
+$lastname.focus(over2);
+$lastname.focusout(out2);
+$address.focus(over3);
+$address.focusout(out3);
+
+$button.click(function() {
+  over1();
+  over2();
+  over3();
+});
+
+});
